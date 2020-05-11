6cac22d863dfcbbac55374b48f1d92145c0d6df3
Antkeo1 <sleav143@gmail.com>
2020-05-11 16:04:46 -0400

    changed my experiences

--- src/Experience.js
+++ src/Experience.js
@@ -1,12 +1,12 @@
-import React from 'react'
-
+import React from "react";
 
 const Experience = () => (
-  <div className='container'>
-    <div >
-      <h4 className='text-danger'>WEB DEVELOPER in TRAINING
-October 2018 to January 2019
-General Assembly | Providence, RI</h4>
+  <div className="container">
+    <div>
+      <h4 className="text-danger">
+        WEB DEVELOPER in TRAINING October 2018 to January 2019 General Assembly
+        | Providence, RI
+      </h4>
       <ul>
         <li>12 weeks, 500 hour, full stack Web Development program</li>
         <li>Built full-stack web applications</li>
@@ -16,42 +16,49 @@ General Assembly | Providence, RI</h4>
     </div>
 
     <div>
-      <h4 className='text-danger'>SOFTWARE DEVELOPER in TRAINING
-June 2018 to September 2018
-Institute of Entrepreneurship and Leadership |
-Providence, Rhode Island</h4>
+      <h4 className="text-danger">
+        SOFTWARE DEVELOPER in TRAINING June 2018 to September 2018 Institute of
+        Entrepreneurship and Leadership | Providence, Rhode Island
+      </h4>
 
       <ul>
         <li>12 week training, introduction to Software Development program</li>
         <li>Introduced to HTML, CSS and Javascript</li>
         <li>Earned certificate of completion</li>
-        <li>Organized development of project using wireframes and user stories</li>
+        <li>
+          Organized development of project using wireframes and user stories
+        </li>
         <li>Used HTML , CSS and Bootstrap to design user interface</li>
         <li>Used Javascript and jQuery to create backend logic</li>
       </ul>
     </div>
 
     <div>
-      <h4 className='text-danger'>
-        Modern React wit Redux, NodeJs and Express online course[2019 update]
-        May 2019 - June 2019
-        Udemy
-      </h4>
-        <ul>
-          <li>73.5 hours of online training</li>
-          <li>Created react applications using react, redux and express</li>
-          <li>Earned certificate of completion</li>
-        </ul>
+      <h4 className="text-danger">Freelace Software Developer</h4>
+      <ul>
+        <li>Promote and run a micro business</li>
+        <li>Attend professional business meetings and network events</li>
+        <li>
+          Created fullstack react applications using react, nodejs, mongoDb and
+          expressjs
+        </li>
+        <li>
+          Successfully completed contracts in a professional and timely manner
+        </li>
+      </ul>
     </div>
 
     <div>
-      <p>PROJECTS:
-        <a href='https://antkeo1.github.io/myTicTacToe/'>Tic Tac Toe |</a>
-        <a href='https://floating-gorge-22160.herokuapp.com/'>Emailer Survey |</a>
-        <a href='https://pvd04-squad06.github.io/Squad-project-client/'>E-Comerce Website</a>
+      <p>
+        PROJECTS:
+        <a href="https://antkeo1.github.io/myTicTacToe/">Tic Tac Toe |</a>
+        <a href="http://uhschool.org">University High School |</a>
+        <a href="https://pvd04-squad06.github.io/Squad-project-client/">
+          E-Comerce Website
+        </a>
       </p>
     </div>
   </div>
-)
+);
 
-export default Experience
+export default Experience;
--- src/Heading.js
+++ src/Heading.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import './Heading.css'
+import React from "react";
+import "./Heading.css";
 
 const Heading = () => (
-  <div className='text-center'>
-    <h1 className='text-danger'>Antonio Keo</h1>
-    <p><mark>35 Union ave, apt 1
-    Providence, RI 02909
-    | Phone: 401-390-6917
-    | E-Mail: keoantonio1@gmail.com
-    | <a href='https://www.github.com/Antkeo1'>My Github page</a>
-    </mark></p>
-
+  <div className="text-center">
+    <h1 className="text-danger">Antonio Keo</h1>
+    <p>
+      <mark>
+        <a href="http://codeworldinc.org">Portfolio</a> | Phone: 401-390-6917 |
+        E-Mail: AntonioKeo@codeworldinc.org |{" "}
+        <a href="https://www.github.com/Antkeo1">My Github page</a>
+      </mark>
+    </p>
   </div>
-)
+);
 
-export default Heading
+export default Heading;
